71d1b9c78bf33e2671c9d5a38df940bb3056db90
SadeshWijekoon <10898706@students.plymouth.ac.uk>
2024-09-04 19:34:18 +0530

    connect the mongooes and / routes

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const userRoute = require("./routes/users");
 dotenv.config()
 
+mongoose.connect(process.env.MONGO_URL, {
+  
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.error('Failed to connect to MongoDB', err));
+
+// middle ware
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("common"))
+
+app.use("/api/users",userRoute)
 
 
-
-app.listen(5000,()=>{
+app.listen('3000',()=>{
     console.log('Server is Running');
     
 })
